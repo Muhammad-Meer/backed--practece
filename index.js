3019a93ced003b6bd1e1d695d79be169ebc57347
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-08 17:34:49 +0500

    new code

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+
+
 app.use(express.json())
 
 
@@ -8,15 +11,23 @@ app.get('/', (req, res) => {
   res.send("Hello World");
 });
 
+
+
 let notes = []
 
 app.post('/notes', (req ,res) => {
   notes.push(req.body)
   res.status(201).json({
-    mesaage: "note created filluy "
+    mesaage: "note created successfully"
   })
 })
 
+
+app.get('/notes', (req ,res) => {
+  res.status(201).json({
+    mesaage: "note created successfully"
+  })
+})
 
 
 app.listen(3200, () => {
