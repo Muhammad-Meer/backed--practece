9a5bea26241e3cf5e08a9c651901df2eac7a6922
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-11 18:24:50 +0500

    new code

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express');
-const { model } = require('mongoose');
+const express = require("express");
+const notemodel = require("./notemodel");
 
-const app = express()
+const app = express();
 
+app.use(express.json());
 
+app.post("/", async (req, res) => {
+  try {
+    const data = req.body;
 
+    await notemodel.create({
+      title: data.title,
+      description: data.description,
+    });
 
+    res.status(201).json({
+      message: "successful"
+    });
 
+  } catch (error) {
+    res.status(500).json({
+      message: "error",
+      error: error.message
+    });
+  }
+});
 
-
-module.exports = app
+module.exports = app;
