c1ec3b3182783ea3a21bf715bdf3e531698a109b
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-12 12:37:43 +0500

    new code

--- app.js
+++ app.js
@@ -5,25 +5,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/", async (req, res) => {
-  try {
-    const data = req.body;
-
-    await notemodel.create({
-      title: data.title,
-      description: data.description,
-    });
-
-    res.status(201).json({
-      message: "successful"
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      message: "error",
-      error: error.message
-    });
-  }
-});
+app.post('/', async ( req, res) => {
+ const data =  req.body
+ await notemodel.create({
+    title: data.title,
+    description: data.description
+ })
+ res.status(201).json({
+    message: "note created successfully"
+ })
+})
 
 module.exports = app;
