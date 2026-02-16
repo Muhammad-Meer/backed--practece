e46b897b8d042c5d9fbb0b73136f01f66c11cf8c
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-16 20:22:39 +0500

    new code

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const mongoose = require('mongoose');
-const app = require('./app');
-
+const mongoose = require("mongoose");
+const app = require("./app");
 
 const port = 3200;
 
+mongoose
+  .connect(process.env.MONGO_URL)
 
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
+  app.listen(port, () => {
+    console.log("http://localhost:" + port)
+  })
