5b9c0a59676ea49a5a7b3157cb8e6206e8216bc4
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-16 20:38:58 +0500

    new code

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
-const app = require("./app");
+
+const mongoose = require('mongoose');
+const app = require('./app');
 
 const port = 3200;
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
-  console.log("Connected to database");
-
-  app.listen(port, () => {
-    console.log("Server running on http://localhost:" + port);
-  });
-
+  console.log('connected to database');
 })
-.catch(err => console.log(err));
+.catch((err) => {
+  console.log(err)
+})
+
+app.listen(port, () => {
+   console.log("http://localhost:" + port);
+})
