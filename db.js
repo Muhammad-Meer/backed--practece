72929224c3e82e0c73dfd2dc189e8e1e9456194c
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-19 13:05:24 +0500

    new code

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-async function connectDb() {
-  await mongoose.connect('mongodb+srv://muhabbatali:zVqoJkgRl0U2kE9V@cluster0.fepov5t.mongodb.net/papan')
-
-  console.log('connected')
-}
-
-module.exports = connectDb
