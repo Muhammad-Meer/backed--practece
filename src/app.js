fe6e5b18f33b035f38baf3fab2e4ff94361560a5
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-21 22:21:18 +0800

    code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,8 @@
-// const express = require("express")
-// const connectDb = require("./db");
+const express = require('express');
 
-// const app = express();
-// const port = 3000;
 
-// // Define a route for GET requests to the root URL (/)
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
 
-// // Start the server and listen on the specified port
-// app.listen(port, () => {
-//   connectDb()
-//   console.log(`Server listening at http://localhost:${port}`);
-// });
+const app = express();
+
+
+module.exports = app;
