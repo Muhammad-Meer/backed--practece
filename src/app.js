6e622011df49502d653d5bedc71f2fecbfc565af
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-22 22:36:09 +0800

    new code

--- src/app.js
+++ src/app.js
@@ -1,17 +1,7 @@
 const express = require('express');
-const postrouter = require('./routes/postRouter');
-const cookies = require('cookie-parser');
-const authroutes = require('./routes/Auth.Routes');
 const app = express();
 
+app.use(express.json());
 
-// 👇 Yeh dono middleware lagao
-app.use(express.json()); // JSON data ke liye
-app.use(express.urlencoded({ extended: true })); // Form data ke liye
-app.use(cookies());
-
-
-app.use('/api/post', postrouter);
-app.use('/api/auth', authroutes);
 
 module.exports = app;
