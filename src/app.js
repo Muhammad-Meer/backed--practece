235e2c9977fb7df9c0aa5ce5caca2e12771340b4
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-22 23:58:40 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cookies = require('cookie-parser');
+const authroure = require('./routes/auth.roure');
 
 
 
 const app = express();
 app.use(express.json());
 app.use(cookies());
+
+
+app.use('/api/auth', authroure)
 
 
 module.exports = app;
