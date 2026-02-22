41e845af9293027ac061a784897691780d0fb222
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-22 22:46:48 +0800

    updated

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cookies = require('cookie-parser');
+
+
+
 const app = express();
 
 app.use(express.json());
+app.use(cookies());
 
 
 module.exports = app;
