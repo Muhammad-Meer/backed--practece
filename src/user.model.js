b250a27268df072e93503e00a7b881de2c1f5270
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-22 20:29:39 +0500

    update

---
+++ src/user.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const userschema = new mongoose.Schema({
+  username: {
+   type: String,
+   required: true,
+   unique: true
+  },
+  email: {
+   type: String,
+   required: true,
+   unique: true
+  },
+})
