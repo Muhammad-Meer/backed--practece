8040e27fe887838400d75be4ef11998c9773321d
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-02-23 13:37:49 +0500

    code

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,51 +1,34 @@
+const { JsonWebTokenError } = require('jsonwebtoken');
 const usermodel = require('../models/user.model');
-const jwt = require("jsonwebtoken");
 
-async function userregister(req, res) {
-
+const userregister = async (req, res) => {
   try {
-    const { username, email, role = "user" } = req.body;
+     
+       const { username, email, role = "user" } = req.body;
 
-    const isuseralreadyexist = await usermodel.findOne({
-      $or: [
-        { username },
-        { email }
-      ]
-    });
+       const isuseralreadyexist = await usermodel.findOne({
+        $or: [{username}, {email}]
+       })
+       
 
-    if (isuseralreadyexist) {
-      return res.status(400).json({
-        message: "User already exist"
-      });
-    }
+       if(isuseralreadyexist) {
+        return res.status(400).json({ message: "user already exist"})
+       }
 
-    const user = await usermodel.create({
-      username,
-      email,
-      role,
-    });
+       const newuser = await usermodel.create({
+       username,
+       email,
+       role,
+       });
 
-    const token = jwt.sign(
-      {
-        id: user._id,
-        role: user.role
-      },
-      process.env.JWT_SECRET
-    );
+       const createtoken = Jwt.sign({
+        id: newuser._id,
+        
+       })
 
-    res.cookie("token", token);
-
-    return res.status(201).json({
-      message: "User registered successfully",
-      user
-    });
 
   } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      message: "internal server error"
-    });
+    res.status(500).json({ message: error.message })
   }
 }
 
-module.exports = { userregister };
